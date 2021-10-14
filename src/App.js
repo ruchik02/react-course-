5531c0bcee2f1b5487955e15d41b3e071a341158
ruchik02 <ruchika02>
2021-10-14 15:07:07 +0530

    error

--- src/App.js
+++ src/App.js
@@ -2,21 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name="Ruchika";
+  
   return (
+    <h1>How are You ?</h1>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World {new Date().toDateString()}
         </p>
-        <a
+        { <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> }
       </header>
     </div>
   );
