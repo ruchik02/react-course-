11dcf212702b9cea52cad0197715bca2df85f77e
ruchik02 <ruchika02>
2021-10-15 11:49:04 +0530

    jsx

--- src/index.js
+++ src/index.js
@@ -4,13 +4,35 @@ import './index.css';
 // import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+  firstName: 'Ruchika',
+  lastName: 'Sharma',
+};
+
+const element = <h1>Hello, {formatName(user)}!</h1>;
+
+ReactDOM.render(element, document.getElementById('root'));
+
+// ReactDOM.render(
   // <React.StrictMode>
   // <App />,  
   // </React.StrictMode>,
-  <h1>Hello World</h1>,
-  document.getElementById('root')
-);
+//   <>
+//   <ul>
+//     <li>Item-1</li>
+//     <li>Item-2</li>
+//     <li>Item-3</li>
+//   </ul>,
+//   <p>Hello Para</p>
+//   </>,
+//   document.getElementById('root')
+// );
+// var fun=document.createElement("p");
+// fun.innerHTML="This is a paragraph"
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
