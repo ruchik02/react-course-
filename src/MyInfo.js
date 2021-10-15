5f211322c90bde956337834380fdc9ce3ae7cd5b
ruchik02 <ruchika02>
2021-10-15 17:03:27 +0530

    MyInfo component

---
+++ src/MyInfo.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function MyInfo(){
+    return (
+      <>
+      <h1>My name is Ruchika Sharma</h1>
+      <p>I'm pursuing engineering from chitkara university my hobby is singing , i like reading books .</p>
+         <ul>
+           <li>Delhi</li>
+          <li>Goa</li>
+         <li>Mumbai</li>
+         <li>Bangalore</li>
+        </ul>
+        <ol>
+          <li>item-1</li>
+          <li>item-2</li>
+          <li>item-3</li>
+          <li>item-4</li>
+        </ol>
+        </>
+    )
+  }
+  export default MyInfo;
