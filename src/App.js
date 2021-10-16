4ad044006e290fef01b63d7cac5935f4b5e739a7
ruchik02 <ruchika02>
2021-10-17 00:07:47 +0530

    card compnent

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import React from "react";
-import Footer from "./Footer";
-import Header from "./Header";
-import './index.css';
-
-function  App(){
+import  ReactDOM  from "react";
+import Card from './Card';
+function App(){
     return(
-    <div className="todo-list">
-      
-    <Header/>
-    {/* <Footer/> */}
-   </div>
-    );
+        <div className="contacts">
+            <Card 
+            img="https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTF8fHxlbnwwfHx8fA%3D%3D&w=1000&q=80"
+            name="hello kitten"
+            phone="hello para"
+            email="hey!"
+            />
+            <Card
+            img="https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTF8fHxlbnwwfHx8fA%3D%3D&w=1000&q=80"
+            name="hello kitten"
+            phone="hello para"
+            email="hey!"
+            />
+            <Card
+            img="https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTF8fHxlbnwwfHx8fA%3D%3D&w=1000&q=80"
+            name="hello kitten"
+            phone="hello para"
+            email="hey!"
+            />
+            <Card
+            img="https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTF8fHxlbnwwfHx8fA%3D%3D&w=1000&q=80"
+            name="hello kitten"
+            phone="hello para"
+            email="hey!"
+            />
+
+         </div>  
+
+            
+    )
 }
 export default App;
-
-
 
---
+++ src/Card.js
@@ -0,0 +1,13 @@
+import React from "react";
+function Card(){
+   return(
+    <div className="contact-cards">
+    <img src=" https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTF8fHxlbnwwfHx8fA%3D%3D&w=1000&q=80" style={{height:'250px',width:'250px'}} />
+    <h3> name:kitten</h3>
+    <p>phone: Hello para</p>
+    <p>email: Hey!</p>
+    </div>
+   )
+}
+
+export default Card;
