1af3664cfde29ae5d5efe06e74e4eb9edb78d123
ruchik02 <ruchika02>
2021-10-17 14:11:07 +0530

    changes in Joke and App component

--- src/App.js
+++ src/App.js
@@ -1,47 +1,39 @@
 import React from "react";
-import  ReactDOM  from "react";
-import Card from './Card';
-function App(){
+import Joke from "./Joke";
+function App() {
     return(
-        <div className="contacts">
-            <Card 
-            contact={{
-                imgUrl:"https://i.pinimg.com/originals/02/e8/60/02e8601a9c05a8540e7fba11c3d7bf0b.jpg",
-            name:" kitten",
-            phone:"8494489488",
-            email:"hey!"
-            }}
-            
-            />
-            <Card
-            contact={{
-                imgUrl:"https://i.pinimg.com/originals/02/e8/60/02e8601a9c05a8540e7fba11c3d7bf0b.jpg",
-                name:" kitten",
-                phone:"8494489488",
-                email:"hey!"
-            }}
-            />
-            <Card
-            contact={{
-                imgUrl:"https://i.pinimg.com/originals/02/e8/60/02e8601a9c05a8540e7fba11c3d7bf0b.jpg",
-            name:" kitten",
-            phone:"8494489488",
-            email:"hey!"
-            }}
-            />
-            <Card
-            contact={{
-                imgUrl:"https://i.pinimg.com/originals/02/e8/60/02e8601a9c05a8540e7fba11c3d7bf0b.jpg",
-                name:" kitten",
-                phone:"8494489488",
-                email:"hey!"
-            }}
-            />
-
-         </div>  
-
-            
+    <div>
+        <Joke
+        // contact={{
+            questions='what is your name ?'
+            answer='My name is ruchika'
+        // }}
+        />
+         <Joke
+        // contact={{
+            questions='How are you ?'
+            answer='I am fine '
+        // }}
+        />
+         <Joke
+        // contact={{
+            questions='what about you ?'
+            answer='I am also good'
+        // }}
+        />
+         <Joke
+        // contact={{
+            questions='Do u like ice-cream?'
+            answer='Yes I like ice-cream'
+        // }}
+        />
+         <Joke
+        // contact={{
+            questions='Have u ate breakfast'
+            answer='Yes i have eaten'
+        // }}
+        />
+    </div>
     )
-}
+ }
 export default App;
-
--- src/Joke.js
+++ src/Joke.js
@@ -2,8 +2,10 @@ import React from "react";
 function Joke(props){
     return(
         <div className="joke-contact">
-        <h3>{props.contact.questions}</h3>
-        <p>{props.contact.answer}</p>
+        <h3> 
+         {props.questions} </h3>
+        <p>{props.answer}</p>
+        <hr/>
        </div>
     )
   
