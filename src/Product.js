5496402e87a3a9686ec0731d7f93ad8fc5ac122d
ruchik02 <ruchika02>
2021-10-18 19:51:43 +0530

    modify setState

--- src/Product.js
+++ src/Product.js
@@ -6,33 +6,69 @@ class Product extends React.Component{
             price:999,
             title:'mobile phone',
             qty:1,
-            img:''
+            img:'',
         }
         // this.increaseQuantity=this.increaseQuantity.bind(this);
+        this.testing();
+    }
+    testing(){
+        const promise=new Promise((resolve,reject)=>{
+            setTimeout(()=>{
+                resolve('done')
+            },5000)
+        });
+        promise.then(()=>{
+            //setState acts like a synchronous call
+            this.setState({qty:this.state.qty+10});
+            this.setState({qty:this.state.qty+10});
+            this.setState({qty:this.state.qty+10});
+            console.log('state',this.state);
+        });
     }
     increaseQuantity= ()=>{
-        // this.state.qty+=1;
-        this.setState({
-            qty:this.state.qty + 1 
-            // title:"Some new title"
+       
+         //form 2 setState if prev state required use this form 
+        this.setState((prevState)=>{
+            return{
+                qty: prevState.qty + 1
+            }
+        },()=>{
+            console.log('this',this.state);
         });
-        //form 2 setState if prev state required use this form 
-
         // this.setState((prevState)=>{
         //     return{
         //         qty: prevState.qty + 1
         //     }
-           
         // });
-        this.setState({
-            qty:this.state.qty-1
-        });
-
-        console.log('this',this.state);
+        // this.setState((prevState)=>{
+        //     return{
+        //         qty: prevState.qty + 1
+        //     }
+        // });
+        // this.setState((prevState)=>{
+        //     return{
+        //         qty: prevState.qty + 1
+        //     }
+        // });
+        
     }
 
+    decreaseQuantity= ()=>{
+        const {qty} =this.state;
+        if(qty===0){
+            return;
+        }
+        this.setState((prevState)=>{
+            return{
+                qty: prevState.qty - 1
+            }
+           
+        });
+        console.log('this',this.state);
+    }
     
     render(){
+        console.log('render');
         const{price,title,qty}=this.state;
         return (
           <div className="product">
@@ -54,6 +90,7 @@ class Product extends React.Component{
                       alt="decrease"
                       className="actions-icons"
                       src="https://cdn-icons-png.flaticon.com/512/66/66889.png"
+                      onClick={this.decreaseQuantity}
                        />
                         <img
                       alt="delete"
